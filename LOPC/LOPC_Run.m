c070b24d9a685b7c20e4618ec988b588f8a5b469
Jason Everett <jaseeverett@gmail.com>
2019-10-28 08:35:59 +1100

    Update to include generic run files

--- LOPC/LOPC_Run.m
+++ LOPC/LOPC_Run.m
@@ -1,25 +1,23 @@
 clear
 close all
 
-% This script is to run LOPC_Analyse from the command line. It inserts all
-% the info which would normally be added from the GUI.
+% This script is to run LOPC_Analyse. It inserts all required info.
 
-LOPC.FileName = 'TestData/LOPC-Logger-McKinnon2.dat';
+LOPC.FileName = '';
 
-LOPC.MinESD = 2.49e-04;
-LOPC.MaxESD = 0.002;
+LOPC.MinESD = 2.49e-04; % Minimum ESD in microns. LOPC min is 100 um
+LOPC.MaxESD = 0.002;  % Maximum ESD in microns. LOPC max is 35 mm
 
 LOPC.MinDepth = 0;
 LOPC.MaxDepth = 500;
 
-LOPC.offset = 10; % number of hours to add to sampling time In this case I want to change it to AEST
+LOPC.offset = 10; % number of hours to add to sampling time In this case I want to change it to AEST from UTC
 
-LOPC.Lat = -64.75;
-
-% LOPC = LOPC_LOPCBins(LOPC);
+LOPC.Lat = -34; % Latitude from which sample was collected. Used for pressure-depth conversion.
 
 LOPC = LOPC_Analyse(LOPC);
 
+figure
 ax = axes;
 OPC_NBSS_Plot(LOPC,1,ax);
 
---
+++ LOPC/LOPC_RunLoop.m
@@ -0,0 +1,122 @@
+clear
+close all
+
+%% 100um net used
+% OPC_NetRetention
+% 3:1 L:W = 173um
+
+data_dir = 'Data';
+figure_dir = 'Figures';
+output_dir = 'Output';
+
+FileList = {'Data/File1.dat'; 'Data/File2.dat'; 'Data/File3.dat'};
+
+fig = 1; % Plot figures or not?
+
+%% Account for splits in the sample. 
+% If no splits, no_splits column should be 0
+% csv file should have form 
+% filename, tow_vol_m3, no_splits
+Split = readtable([data_dir,filesep,'LOPC_Metadata.csv']);
+
+%% Process Files
+for a = 1:length(FileList)
+    
+    LOPC.MinESD = 200/1e6;
+    LOPC.Param.Ellipsoid = 3; % What ratio of Major:Minor axis of plankton to assume
+
+    LOPC.MaxESD = 30000/1e6; % 30 mm - Theoretical max is 35 mm
+    LOPC.NBSS.min_count = 1;
+    
+LOPC.offset = 10; % number of hours to add to sampling time In this case I want to change it to AEST from UTC
+
+LOPC.Lat = -34; % Latitude from which sample was collected. Used for pressure-depth conversion.
+
+    LOPC.FileName = FileList{a};
+    sf = strfind(LOPC.FileName,'/');
+    
+    LOPC.ShortName = LOPC.FileName(sf(end)+1:end);
+    
+    %%
+    % Match up the splits with the files
+    s = find(strcmp(Split.filename, LOPC.ShortName(1:end-4))==1);
+    if isempty(s)
+        error('s empty')
+    end
+    
+    LOPC.tow_vol = Split.tow_vol_m3(s);
+    LOPC.Split = 1/(2^Split.no_splits(s));
+    LOPC.vol = LOPC.tow_vol * LOPC.Split;
+    
+    %%
+    % Flow flag. Do not change for Lab-LOPC
+    flow_mark = 3; % Use given volume
+    
+    %% Process LOPC
+    LOPC = LOPC_Analyse(LOPC);
+    
+    % Plot NBSS
+    if fig == 1
+        ax = axes;
+        txt = 12;
+        Lim = [10^-3 10^3 10^-2 10^4];
+        
+        h = OPC_NBSS_Plot(LOPC,Lim,ax,1,txt);
+        
+        try
+            h.h3.Visible = 'off';
+            h.nlin_CI.Visible = 'off';
+        catch
+        end
+        
+        ti = title(LOPC.ShortName(1:end-4),'Interpreter','none');
+        set(gcf,'color','w')
+        
+        warning off
+        out_name = [figure_dir,filesep,LOPC.ShortName(1:end-4)];
+        export_fig(out_name,'-pdf')
+        
+        out_name = [figure_dir,filesep,'All_Sites'];
+        
+        if a ==1
+            export_fig(out_name,'-pdf')
+        else
+            export_fig(out_name,'-pdf','-append')
+        end
+        warning on
+        close all
+           
+    end
+    
+    save([output_dir,filesep,LOPC.ShortName(1:end-4),'.mat'],'LOPC')
+    
+    LOPC_OutputCSV(LOPC,[output_dir,filesep,LOPC.ShortName(1:end-4)])
+    
+    %%
+    Compile.FileName{a,1} = LOPC.ShortName;
+    
+    Compile.Volume(a,1) = LOPC.Flow.TotalVol;
+    Compile.Split(a,1) = LOPC.Split;
+    Compile.MinESD(a,1) = LOPC.MinESD*1e6;
+    Compile.MaxESD(a,1) = LOPC.MaxESD*1e6;
+    
+    Compile.ParetoSlope(a,1) = LOPC.Pareto.Slope;
+    Compile.NBSS_Slope(a,1) = LOPC.NBSS.Lin.Slope;
+    Compile.NBSS_Intercept(a,1) = LOPC.NBSS.Lin.Intercept;
+    Compile.NBSS_r2(a,1) = LOPC.NBSS.Lin.r2;
+    
+    Compile.Total_Counts(a,1) = LOPC.Stats.Total_Counts;
+    Compile.Abundance_indm3(a,1) = LOPC.Stats.Abundance;
+    Compile.Biomass_mgm3(a,1) = LOPC.Stats.Biomass;
+    Compile.GeometricMeanSize(a,1) = LOPC.Stats.GeoMn.*1e6;
+           
+    
+    clear LOPC
+end
+
+
+tbl = struct2table(Compile);
+writetable(tbl,[output_dir,filesep,'LOPC_Summary.csv']);
+
+
+
